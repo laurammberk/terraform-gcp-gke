abe19cb1ec1c5abd3fa1b35eb8c757f41b7b10cb
laurammberk <laurammberk3105@gmail.com>
2026-08-11 02:24:30 +0000

    removed node block

--- main.tf
+++ main.tf
@@ -13,11 +13,6 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  node_config {
-    machine_type = var.gke_config["machine_type"]
-    disk_size_gb = var.gke_config["disk_size_gb"]
-    disk_type    = "pd-standard"
-  }
   cluster_autoscaling {
     enabled             = false
     autoscaling_profile = "OPTIMIZE_UTILIZATION"
